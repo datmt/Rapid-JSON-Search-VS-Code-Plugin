a9cea764fc244697d476ed620b529201d12250ef
dungtt <t2dx.inc@gmail.com>
2021-11-05 19:33:17 +0700

    apply prettier

--- src/extension.ts
+++ src/extension.ts
@@ -1,191 +1,183 @@
-'use strict';
+"use strict";
 
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
+  const writeEmitter = new vscode.EventEmitter<string>();
 
+  let jsonText = "some";
+  let terminalName = "JSON Search";
+  let terminal;
 
-	const writeEmitter = new vscode.EventEmitter<string>();
+  // let activeEditor = vscode.window.activeTextEditor;
 
+  context.subscriptions.push(
+    vscode.commands.registerCommand("rapidJSONSearch.create", () => {
+      let activeEditor = vscode.window.activeTextEditor;
+      if (activeEditor) {
+        jsonText = activeEditor.document.getText();
+        // terminalName = activeEditor.document.fileName;
+      } else {
+        return;
+      }
 
-	let jsonText = 'some';
-	let terminalName = 'JSON Search';
-	let terminal;
+      let line = "";
+      writeEmitter.fire("\x1b[2J\x1b[3J\x1b[;H");
 
-	// let activeEditor = vscode.window.activeTextEditor;
+      let jsObject = JSON.parse("{}");
+      try {
+        jsObject = JSON.parse(jsonText);
+      } catch (ex) {}
 
-	context.subscriptions.push(vscode.commands.registerCommand('rapidJSONSearch.create', () => {
-		let activeEditor = vscode.window.activeTextEditor;
-		if (activeEditor) {
-			jsonText = (activeEditor.document.getText());
-			// terminalName = activeEditor.document.fileName;
-		} else {
-			return;
-		}
+      let pty = {
+        onDidWrite: writeEmitter.event,
+        open: () =>
+          writeEmitter.fire("Type and press enter to search:\r\n\r\n"),
+        close: () => {},
+        handleInput: (data: string) => {
+          if (data === "\r") {
+            // Enter
 
-		let line = '';
-		writeEmitter.fire('\x1b[2J\x1b[3J\x1b[;H');
+            //clear the teminal on :clear
+            if (line.trim() === ":clear") {
+              line = "";
 
-		let jsObject = JSON.parse('{}');
-		try {
-			jsObject = JSON.parse(jsonText);
-		} catch (ex) {
+              if (typeof terminal !== "undefined") {
+                writeEmitter.fire("\x1b[2J\x1b[3J\x1b[;H");
+                return;
+              }
+            }
 
-		}
+            if (line.trim() === ":kaput") {
+              line = "";
+              if (typeof terminal !== "undefined") {
+                terminal.dispose();
+                terminal = null;
+                return;
+              }
+            }
 
+            if (line.trim() === ":reload") {
+              try {
+                line = "";
+                jsonText = activeEditor.document.getText();
+                jsObject = JSON.parse(jsonText);
+                writeEmitter.fire("\x1b[2J\x1b[3J\x1b[;H");
+                return;
+              } catch (e) {
+                console.log(e);
+                return;
+              }
+            }
 
+            let result = findVal(jsObject, line);
 
-		let pty = {
-			onDidWrite: writeEmitter.event,
-			open: () => writeEmitter.fire('Type and press enter to search:\r\n\r\n'),
-			close: () => { },
-			handleInput: (data: string) => {
-				if (data === '\r') { // Enter
+            console.log("results are: ", result);
+            writeEmitter.fire(`\r\nResult: ${result[0]}\r\n`);
+            writeEmitter.fire(`\r\nMatch paths:\r\n   `);
 
-					//clear the teminal on :clear
-					if (line.trim() === ':clear') {
-						line = '';
+            if (Array.isArray(result[1])) {
+              for (let p in result[1]) {
+                let jsPath = result[1][p].substr(1);
+                let pyPath = convertToPythonPath(jsPath);
+                let res = jsPath + " (JS) or " + pyPath + " (Python)";
+                writeEmitter.fire(`${res}\r\n   `);
+              }
+            }
 
-						if (typeof terminal !== 'undefined') {
+            writeEmitter.fire("\r\n------------------------\r\n");
+            writeEmitter.fire("Type and press enter to search:\r\n\r\n");
+            line = "";
 
-							writeEmitter.fire('\x1b[2J\x1b[3J\x1b[;H');
-							return;
-						}
-					}
+            return;
+          }
+          if (data === "\x7f") {
+            // Backspace
+            if (line.length === 0) {
+              return;
+            }
+            line = line.substr(0, line.length - 1);
+            // Move cursor backward
+            writeEmitter.fire("\x1b[D");
+            // Delete character
+            writeEmitter.fire("\x1b[P");
+            // writeEmitter.fire(v1.window.activeTextEditor?.document.fileName);
+            return;
+          }
 
-					if (line.trim() === ':kaput') {
-						line = '';
-						if (typeof terminal !== 'undefined') {
-							terminal.dispose();
-							terminal = null;
-							return;
-						}
-					}
+          line += data;
+          console.log("line now is: ", line);
+          writeEmitter.fire(data);
+        },
+      };
 
-					if (line.trim() === ':reload') {
-						try {
-							line = '';
-							jsonText = (activeEditor.document.getText());
-							jsObject = JSON.parse(jsonText);
-							writeEmitter.fire('\x1b[2J\x1b[3J\x1b[;H');
-							return;
-						} catch (e) {
-							console.log(e);
-							return;
-						}
+      if (typeof terminal === "undefined" || terminal == null)
+        terminal = (<any>vscode.window).createTerminal({
+          name: terminalName,
+          pty,
+        });
+      terminal.show();
+    })
+  );
 
-					}
-
-					let result = findVal(jsObject, line);
-
-
-					console.log('results are: ', result);
-					writeEmitter.fire(`\r\nResult: ${result[0]}\r\n`);
-					writeEmitter.fire(`\r\nMatch paths:\r\n   `);
-
-					if (Array.isArray(result[1])) {
-						for (let p in result[1]) {
-							let jsPath = result[1][p].substr(1);
-							let pyPath = convertToPythonPath(jsPath);
-							let res = jsPath + " (JS) or " + pyPath + " (Python)";
-							writeEmitter.fire(`${res}\r\n   `);
-						}
-					}
-
-					writeEmitter.fire('\r\n------------------------\r\n')
-					writeEmitter.fire('Type and press enter to search:\r\n\r\n')
-					line = '';
-
-					return;
-				}
-				if (data === '\x7f') { // Backspace
-					if (line.length === 0) {
-						return;
-					}
-					line = line.substr(0, line.length - 1);
-					// Move cursor backward
-					writeEmitter.fire('\x1b[D');
-					// Delete character
-					writeEmitter.fire('\x1b[P');
-					// writeEmitter.fire(v1.window.activeTextEditor?.document.fileName);
-					return;
-				}
-
-				line += data;
-				console.log('line now is: ', line);
-				writeEmitter.fire(data);
-
-
-			}
-		};
-
-
-
-		if (typeof terminal === "undefined" || terminal == null)
-			terminal = (<any>vscode.window).createTerminal({ name: terminalName, pty });
-		terminal.show();
-	}));
-
-	context.subscriptions.push(vscode.commands.registerCommand('rapidJSONSearch.clear', () => {
-		writeEmitter.fire('\x1b[2J\x1b[3J\x1b[;H');
-	}));
+  context.subscriptions.push(
+    vscode.commands.registerCommand("rapidJSONSearch.clear", () => {
+      writeEmitter.fire("\x1b[2J\x1b[3J\x1b[;H");
+    })
+  );
 }
 
 function convertToPythonPath(jsPath) {
-	let pyPath = (jsPath.replace(/[a-z]+/g, '["$&"]')).replace(/\./g, '');
-	return pyPath;
+  let pyPath = jsPath.replace(/[a-z]+/g, '["$&"]').replace(/\./g, "");
+  return pyPath;
 }
 
 function getPaths(data) {
-	var validId = /^[a-z_$][a-z0-9_$]*$/i;
-	var result = [];
-	doIt(data, "");
-	return result;
+  var validId = /^[a-z_$][a-z0-9_$]*$/i;
+  var result = [];
+  doIt(data, "");
+  return result;
 
-	function doIt(data, s) {
-		if (data && typeof data === "object") {
-			if (Array.isArray(data)) {
-				for (var i = 0; i < data.length; i++) {
-					doIt(data[i], s + "[" + i + "]");
-				}
-			} else {
-				for (var p in data) {
-					if (validId.test(p)) {
-						doIt(data[p], s + "." + p);
-					} else {
-						doIt(data[p], s + "[\"" + p + "\"]");
-					}
-				}
-			}
-		} else {
-			result.push(s);
-		}
-	}
+  function doIt(data, s) {
+    if (data && typeof data === "object") {
+      if (Array.isArray(data)) {
+        for (var i = 0; i < data.length; i++) {
+          doIt(data[i], s + "[" + i + "]");
+        }
+      } else {
+        for (var p in data) {
+          if (validId.test(p)) {
+            doIt(data[p], s + "." + p);
+          } else {
+            doIt(data[p], s + '["' + p + '"]');
+          }
+        }
+      }
+    } else {
+      result.push(s);
+    }
+  }
 }
 
 function findVal(jsonObject: JSON, searchTerms: string) {
-	let allPaths = getPaths(jsonObject);
-	let searchTree = searchTerms.split(".");
+  let allPaths = getPaths(jsonObject);
+  let searchTree = searchTerms.split(".");
 
-	let currentObj = jsonObject;
-	for (let i = 0; i < searchTree.length; i++) {
-		if (typeof currentObj[searchTree[i]] === 'undefined') {
-			currentObj = null;
-			break;
-		}
-		currentObj = currentObj[searchTree[i]];
-	}
+  let currentObj = jsonObject;
+  for (let i = 0; i < searchTree.length; i++) {
+    if (typeof currentObj[searchTree[i]] === "undefined") {
+      currentObj = null;
+      break;
+    }
+    currentObj = currentObj[searchTree[i]];
+  }
 
-	console.log(currentObj, ' is current');
-	let result = '';
-	if (currentObj !== null)
-		result = JSON.stringify(currentObj);
+  console.log(currentObj, " is current");
+  let result = "";
+  if (currentObj !== null) result = JSON.stringify(currentObj);
 
-	//find the possible path
-	let matchPaths = allPaths.filter(v =>
-		v.indexOf(searchTerms) > -1
-	)
+  //find the possible path
+  let matchPaths = allPaths.filter((v) => v.indexOf(searchTerms) > -1);
 
-	return [result, matchPaths];
-
+  return [result, matchPaths];
 }
